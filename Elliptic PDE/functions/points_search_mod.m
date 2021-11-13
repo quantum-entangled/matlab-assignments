404491049465f08335c00a3bfc1aa0685d79714d
Ivan Shalamov <i.y.shalamov@gmail.com>
2021-11-13 20:47:01 +0300

    Update .gitignore

---
+++ Elliptic PDE/functions/points_search_mod.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = points_search(inputArg1,inputArg2)
+%POINTS_SEARCH Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
